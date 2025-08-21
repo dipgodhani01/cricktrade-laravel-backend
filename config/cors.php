afbfbd33439dc6987be4f03ee8a3b8f9c831178a
dip godhani <dipgodhani1845@gmail.com>
2025-08-21 16:03:21 +0530

    initial update cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,12 @@
 return [
     'paths' => ['api/*', '*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'https://your-frontend-domain.com',
+        'https://cricktrade-laravel-backend.onrender.com'
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
